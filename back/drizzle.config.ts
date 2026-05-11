a0241d8326aeeb0e4ae4b4ca4256563f9e20e745
shion <shion48691811@gmail.com>
2026-05-11 11:32:47 +0900

    わからないところが多いのでコメントアウトの追加 AWSの SSL検証をスキップするためのコードを追加

--- back/drizzle.config.ts
+++ back/drizzle.config.ts
@@ -1,7 +1,12 @@
+// スキーマを参照しマイグレーション実行するためのファイル
+// npx drizzle-kit generate → スキーマからSQLを自動生成
+// npx drizzle-kit migrate → 実際にDBに反映
+
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  //   スキーマの場所
+  //   スキーマの参照場所
   schema: "./src/db/schema.ts",
   //   マイグレーションファイルの出力先
   out: "./drizzle",
@@ -9,5 +14,6 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: { rejectUnauthorized: false },
   },
 });
